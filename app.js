39a4e7d393dedab11e4ba97d68efda8028d773d9
takuto-okamoto <okamotot@sheridancollege.ca>
2021-05-08 14:12:40 -0400

    Connected to Database

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+
+// Connect to database
+const dbUrl = process.env.DB_URL;
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+// Connect to mongoose
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
 
 const app = express();
 
+app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+
+// Connect to port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Serving on port ${port}`)
-})
+});
